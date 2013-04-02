f164436fc44714a8b0bff48d3c550cd76c40cf9c
Iván Lòpez <ivlo@MacBook-Pro-de-Ivan-3.local>
2013-04-02 15:29:42 +0200

    ejercicio terminado

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,17 +2,10 @@
 var Filters = (function(){
   var Filters = function(element){
     var self = this;
-
-    this.prop = ko.observable(1)
-
-    this.color = ko.observableArray();
-    this.text_case = ko.observableArray();
-    this.importance = ko.observableArray();
-
-    // element.find("[data-filter-name]").each(function(){
-    //   var filter_type = $(this).data("filter-name");
-    //   self[filter_type] = ko.observableArray();
-    // });
+    element.find("[data-filter-name]").each(function(){
+      var filter_type = $(this).data("filter-name");
+      self[filter_type] = ko.observableArray();
+    });
   }
 
   $.extend(Filters.prototype,{
@@ -21,34 +14,34 @@ var Filters = (function(){
       var target = $(event.target), // input[radio]
           filters_group = $(event.currentTarget), // fieldset
           val = target.val(),
-          collection = this.color, //self[filters_group.data("filter-name")],
+          collection = self[filters_group.data("filter-name")],
           index = collection.indexOf(val),
-          filter_type = "color" //filters_group.data("filter-type") || "or";
-      // if(filter_type == "and"){
-      //   var args = [0, collection().length];
-      //   if(val) args.push(val);
-      //   collection.splice.apply(collection,args);
-      // }else{
+          filter_type = filters_group.data("filter-type") || "or";
+      if(filter_type == "and"){
+        var args = [0, collection().length];
+        if(val) args.push(val);
+        collection.splice.apply(collection, args);
+      }else{
          if(target.is(":checked")){
            if(index == -1)
              collection.push(val);
          }else if(index != -1)
            collection.splice(index,1);
-      // }
+      }
       
     },
     formatted_color: function() {
       return this.color().sort().join(" | ");
+    },
+    filter_by: function(element){
+      var filters = [].slice.call(arguments, 1),
+          self = this;
+      element = $(element);
+      return _(filters).every(function(filter){
+        var filter_type = self[filter]();
+        return filter_type.length == 0 || filter_type.indexOf(element.data(filter)) != -1;
+      });
     }
-    // filter_by: function(element){
-    //   var filters = [].slice.call(arguments, 1),
-    //       self = this;
-    //   element = $(element);
-    //   return _(filters).every(function(filter){
-    //     var filter_type = self[filter]();
-    //     return filter_type.length == 0 || filter_type.indexOf(element.data(filter)) != -1;
-    //   });
-    // }
   });
 
   return Filters;
